a7bb5b404b173b9a5541cde98729bbffb3403310
Narasimha lakamsani <nc.lakamsani@gmail.com>
2021-05-17 12:08:28 -0400

    Update onnx_coverter.py

--- onnx_coverter.py
+++ onnx_coverter.py
@@ -1,12 +1,15 @@
 import torch
 import predict_helper
 
+#Load the model from checkpoint
 model = predict_helper.load_checkpoint("checkpoint.pth")
 
+#Create a dummy input
 model.to('cpu')
 image = predict_helper.process_image("img4.jpg")
 dummy_input = image.unsqueeze_(0).to('cpu').float()
 
+#export model to onnx
 torch.onnx.export(model,
                   dummy_input,
                   "model.onnx")

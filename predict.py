9a6ec8dfb8404d4cb62561998724cf1700336906
Narasimha lakamsani <nc.lakamsani@gmail.com>
2021-06-03 12:25:54 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,14 +1,16 @@
 # Make imports
-import predict_helper
 import argparse
 
+import predict_helper
+
 parser = argparse.ArgumentParser(description="Neural Network Settings for prediction")
-parser.add_argument('image', type=str)
+parser.add_argument('image', type=str, nargs="+")
 parser.add_argument('--checkpoint', type=str, default='./checkpoint.pth')
-parser.add_argument('--top_k', type=int, default=1)
+parser.add_argument('--top_k', type=int, default=2)
 parser.add_argument('--gpu', action="store_true", default=False)
 args = parser.parse_args()
     
 model = predict_helper.load_checkpoint(args.checkpoint)
 
-predict_helper.image_predict(args.image, model, topk=args.top_k, gpu=args.gpu)
+for images in args.image:
+    predict_helper.image_predict(images, model, topk=args.top_k, gpu=args.gpu)
